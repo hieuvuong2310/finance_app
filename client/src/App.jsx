981f7d8e81ec5e53e55171fbce7f8c6c7c1ed61c
Trong Hieu Vuong <hieuvg02@gmail.com>
2023-06-09 08:20:38 -0700

    update files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,29 @@
-import { useState } from 'react'
-import SignUpPage  from './components/pages/SignUpPage'
-import SignInPage  from './components/pages/SignInPage'
-// import TransactionPage  from './components/pages/TransactionPage'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css'
-import TransactionPage from './components/pages/TransactionsPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignUpPage from './components/pages/SignUpPage/SignUpPage';
+import SignInPage from './components/pages/SignInPage/SignInPage';
+import WalletsPage from './components/pages/WalletsPage/WalletsPage';
+import Transaction from './components/Layout/AddTransactionComponent/Transaction';
+import TransactionPage from './components/pages/TransactionPage/TransactionsPage';
+import WalletCard from './components/Layout/WalletCard/WalletCard';
+import HistoryCard from './components/Layout/HistoryCard/HistoryCard';
+import AddWallet from './components/Layout/AddWallet/AddWallet';
+import './App.css';
 
 function App() {
   return (
     <>
-    <div>
+
+    {/* TODO these routes are just for testing purpose, delete the routes after done. */}
       <Router>
         <Routes>
-          <Route path="/" element={<SignInPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/transactions" element={<TransactionPage />} />
+          <Route path='/' element={<SignInPage />} />
+          <Route path='/signup' element={<SignUpPage />} />
+          <Route path='/transactions' element={<TransactionPage />} />
+          <Route path='/wallets' element={<WalletsPage />} />
         </Routes>
       </Router>
-    </div>
-    {/* <div> 
-      <SignInPage/>
-    </div> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
